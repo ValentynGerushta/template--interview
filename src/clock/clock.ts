a0d491169503bfd1fb82fd57ded268e1039af631
Valentyn <valentyn@macbook-pro-de-valentyn.home>
2022-01-09 22:14:47 +0100

    solution1

---
+++ src/clock/clock.ts
@@ -0,0 +1,65 @@
+export enum Edit {
+  Hour = 0,
+  Minute
+}
+
+export class Clock {
+
+  deltaHours: number;
+  deltaMinutes: number;
+  mode: number; // 3 modes: editable hour, editable minute or non-editable
+
+  constructor(){
+    this.deltaHours = 0;
+    this.deltaMinutes = 0;
+    this.mode = 0;
+  }
+
+  // get current mode
+  public getMode(): number{
+    return this.mode;
+  }
+
+  public getHours(): string{
+    const date: Date = new Date();
+    date.setHours(date.getHours() + this.deltaHours);
+    return this.formatTime(date.getHours());
+  }
+
+  public getMinutes(): string{
+    const date: Date = new Date();
+    date.setMinutes(date.getMinutes() + this.deltaMinutes);
+    return this.formatTime(date.getMinutes());
+  }
+
+  public getSeconds(): string{
+    const date: Date = new Date();
+    return this.formatTime(date.getSeconds());
+  }
+
+  // format to 2 digit number
+  private formatTime(time: number): string{
+    if ( time < 10 ) {
+      return '0' + time;
+    }
+    return '' + time;
+  }
+
+  // change mode
+  public nextMode(): void{
+    this.mode++;
+    this.mode %= 3; // only 3 modes
+  }
+
+  // add hour or minute when time is editable
+  public increaseTime(): void{
+    if(this.mode === Edit.Hour){
+      this.deltaHours += 1;
+      this.deltaHours %= 24; // 1day = 24h
+    }
+    else if(this.mode === Edit.Minute){
+      this.deltaMinutes += 1;
+      this.deltaHours %= 60; // 1h = 60min
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,68 @@
 import './index.css';
-import { MyClass } from './example-unit';
+import {Clock, Edit} from './clock';
 
-const a = new MyClass(2);
-console.log('number is', a.get());
+let lightDuration: number = 3000; // 3 seconds
+let startTimer: number = 0, stopTimer: number = 0;
+let lightEnabled: boolean = false;
+const clock: Clock = new Clock();
+
+const displayHour = document.getElementById('timeHour');
+const displayMin = document.getElementById('timeMin');
+const displaySec = document.getElementById('timeSec');
+const buttonMode = document.getElementById("buttonMode");
+const buttonIncrease = document.getElementById("buttonIncrease");
+const buttonLight = document.getElementById("buttonLight");
+const display = document.getElementById("display");
+
+function updateDisplay(): void{
+    displayHour.innerText = clock.getHours();
+    displayMin.innerText = clock.getMinutes();
+    displaySec.innerText = clock.getSeconds();
+
+    // turn off the display light when time is out
+    if(lightEnabled === true){
+        stopTimer = Date.now() - startTimer;
+        if(stopTimer > lightDuration){
+            lightEnabled = false;
+            display.style.backgroundColor = "lightgrey"; // turn off the display light
+        }
+    }
+}
+
+buttonMode.onclick = () => {
+    clock.nextMode(); // increment mode
+
+    // hour is editable
+    if(clock.getMode() === Edit.Hour){
+        // only hour part is pulsing
+        displayHour.style.animation = "PULSE 1s infinite";
+        displayMin.style.animation = "none";
+    }
+    // minute is editable
+    else if(clock.getMode() === Edit.Minute){
+        // only minute part is pulsing
+        displayHour.style.animation = "none";
+        displayMin.style.animation = "PULSE 1s infinite";
+    }
+    // time is not editable
+    else{
+        // no pulsing, all is static
+        displayHour.style.animation = "none";
+        displayMin.style.animation = "none";
+    }
+    updateDisplay();
+}
+
+buttonIncrease.onclick = () => {
+    // add 1 hour or 1 minute, if time is editable
+    clock.increaseTime();
+    updateDisplay();
+}
+
+buttonLight.onclick = () => {
+    display.style.backgroundColor = "yellow"; // turn on the display light
+    lightEnabled = true;
+    startTimer = Date.now();
+}
+
+setInterval(updateDisplay, 1000); // update each second
